5b4427aaa14bcbbcfdf5f16541970540daa2f2e5
abidi <safa.abidi99@gmail.com>
2021-08-03 15:28:02 +0100

    custom category

---
+++ android/app/src/main/kotlin/com/example/gatgory/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gatgory
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'Category.dart';
+
+const _categoryName = 'Cake';
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
+
+void main() {
+  runApp(UnitConverterApp());
+}
+
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner : false,
+      title: "Unit Converter",
+      home : Scaffold(
+        backgroundColor : Colors.green[100],
+        body: Center(
+          child: Category(
+            name: _categoryName,
+            color: _categoryColor,
+            iconLocation: _categoryIcon,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
