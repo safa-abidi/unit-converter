97b675d39feffe89e5cda6d570c06e48ac252c7a
abidi <safa.abidi99@gmail.com>
2021-08-03 15:48:53 +0100

    more categories ! and routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'Category.dart';
+import 'category_route.dart';
 
-const _categoryName = 'Cake';
-const _categoryIcon = Icons.cake;
-const _categoryColor = Colors.green;
 
 void main() {
   runApp(UnitConverterApp());
@@ -15,16 +12,7 @@ class UnitConverterApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner : false,
       title: "Unit Converter",
-      home : Scaffold(
-        backgroundColor : Colors.green[100],
-        body: Center(
-          child: Category(
-            name: _categoryName,
-            color: _categoryColor,
-            iconLocation: _categoryIcon,
-          ),
-        ),
-      ),
+      home : CategoryRoute(),
     );
   }
 }
